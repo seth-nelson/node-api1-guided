4fc9c79cff0da2f0fb30c9cb032a60a6be573f1e
Seth Nelson <seth.nelson@outlook.com>
2020-05-10 10:14:04 -0700

    added files and created a succesful server that responds to calls per guided project

--- index.js
+++ index.js
@@ -1,6 +1,91 @@
 const express = require('express');
+const shortid = require('shortid');
 
 const server = express();
+
+server.use(express.json())
+
+let hubs = [];
+let lessons = [];
+
+server.get('/', (req, res) => {
+    res.json({message: 'hello world!'});
+})
+
+// POST ----------------------------------------------------------
+
+server.post('/api/hubs', (req, res) => {
+    const hubInfo = req.body;
+    console.log('hubInfo', hubInfo)
+
+    hubInfo.id = shortid.generate();
+    hubs.push(hubInfo);
+
+    res.status(201).json(hubInfo);
+});
+
+// READ -----------------------------------------------------------
+
+server.get('/api/hubs', (req, res) => {
+    res.status(200).json(hubs);
+})
+
+// DELETE ----------------------------------------------------------
+
+server.delete('/api/hubs/:id', (req, res) => {
+    const { id } = req.params
+    console.log(id);
+
+    const found = hubs.find(hub => hub.id === id);
+
+    if (found) {
+        hubs = hubs.filter(hub => hub.id !== id)
+        res.status(200).json(found);
+    } else {
+        res.status(404).json({ message: 'hub not found' });
+    }
+
+    res.status(201).json({ message: 'I did nothing' });
+})
+
+
+// UPDATE -------------------------------------------------
+
+
+server.patch('/api/hubs/:id', (req, res) => {
+    const { id } = req.params;
+    const changes = req.body
+
+    let found = hubs.find(hub => hub.id === id);
+
+    if (found) {
+        Object.assign(found, changes);
+        res.status(200).json(found);
+    } else {
+        res.status(404).json({ message: 'hub not found' });
+    }
+})
+
+
+// UPDATE PUT ---------------------------------------------
+
+
+server.put('/api/hubs/:id', (req, res) => {
+    const { id } = req.params;
+    const changes = req.body
+
+    let index = hubs.findIndex(hub => hub.id === id);
+
+    if (index !== -1) {
+        changes.id = id;
+        hubs[index] = changes
+        res.status(200).json(hubs[index]);
+    } else {
+        res.status(404).json({ message: 'hub not found' });
+    }
+})
+
+
 
 const PORT = 5000;
 
